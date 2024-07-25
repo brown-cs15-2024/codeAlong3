a446fec7b5813457a6c8b378df852485a1761f7e
brathier28 <144404108+brathier28@users.noreply.github.com>
2024-07-24 20:30:15 -0400

    Apple.java

--- Apple.java
+++ Apple.java
@@ -1,5 +1,18 @@
 package codeAlong3;
 
 public class Apple {
-
+  
+  public void shuffleAppleMusic(){
+    int randInt = (int) (Math.random() * 5);
+    case 0:
+      print("Playing song1 by artist!");
+    case 1:
+      print("Playing song2 by artist!");
+    case 2:
+      print("Playing song3 by artist!");
+    case 3:
+      print("Playing song4 by artist!");
+    default:
+      print("Playing song5 by artist!");
+  }
 }
